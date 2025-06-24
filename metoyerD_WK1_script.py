260cfa9e625efcb2ed569a31b578636ba3bbbe18
DDMetoyer <76956108+DDMetoyer@users.noreply.github.com>
2025-06-24 14:36:47 -0700

    Update metoyerD_WK1_script.py

--- metoyerD_WK1_script.py
+++ metoyerD_WK1_script.py
@@ -1,32 +1,16 @@
 '''
-Devante Metoyer
-01/25/25
-Assignment 1
-Week One Assignment - Simple String Searching 
-'''
-
-'''
-Given excerpt from the hacker manifesto 
-
-Complete the script below to do the following:
-1) Add your name, date, assignment number to the top of this script
-2) Convert the string to all lower case
-3) Count the number characters in the string
-4) Count the number of words in the string
-5) Sort the words in alphabetical order
-6) Search the excerpt variable given below
+Simple String Searching:
+1) Convert the string to all lower case
+2) Count the number characters in the string
+3) Count the number of words in the string
+4) Sort the words in alphabetical order
+5) Search the excerpt variable given below
    For the following and report how many occurances of each are found
    scandal
    arrested
    er
    good
    tomorrow
-7) Submit
-   NamingConvention: lastNameFirstInitial_Assignment_.ext
-   for example:  alharthiD_WK1_script.py
-                 alharthiD_WK1_screenshot.jpg
-   A) Screenshot of the results in WingIDE
-   B) Your Script
 '''
 
 excerpt = " Another one got caught today, it's all over the papers. Teenager Arrested in Computer Crime Scandal, Hacker Arrested after Bank Tampering kids. They're all alike"
